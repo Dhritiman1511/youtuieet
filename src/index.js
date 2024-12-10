fc4e365c139bec487805ede0fc49ce15c11679b4
Dhritiman1511 <128951205+Dhritiman1511@users.noreply.github.com>
2024-12-11 00:36:03 +0530

    multer

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import connectDB from './db/index.js';
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: './.env'
+});
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8080,()=>{
+        console.log(`Server running at port ${process.env.PORT}`);
+    });
+})
+.catch((error)=>{
+    console.log("MongoDB db connection failed",error);
+})
